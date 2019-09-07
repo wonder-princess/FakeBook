4caf1c61aab90b5f30876f0c44204280bd68a7e9
wonder-princess <54895094+wonder-princess@users.noreply.github.com>
2019-09-08 00:34:19 +0900

    Add files via upload

--- test.java
+++ test.java
@@ -6,75 +6,65 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 
-public class Test extends JFrame /*implements ActionListener*/{
+public class Test extends JFrame {
     private static final long serialVersionUID = 1L;
+    JPanel loginDisp = new JPanel();
+    JPanel homeDisp = new JPanel();
+    JPasswordField password = new JPasswordField('*');
     public static void main(String args[]) {
         new Test();
     }
 
-    private JPanel mainPanel;
-    JPasswordField password = new JPasswordField('*');
-
-    private static final String PANEL_A = "画面A";
-    private static final String PANEL_B = "画面B";
-
     public Test() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
-        setLocationRelativeTo(null);
-
-        //複数のパネルを乗せ, 名前で切り替える土台となるパネル
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new CardLayout()); //CardLayoutは重なったパネルを切り替えることができます.
-
-        //1枚目
-        JPanel panelA = createPanelA();
-        mainPanel.add(panelA, PANEL_A);
-
-        //2枚目
-        JPanel panelB = createPanelB();
-        mainPanel.add(panelB, PANEL_B);
-
-        //起動時は一枚目を表示
-        ((CardLayout)(mainPanel.getLayout())).show(mainPanel, PANEL_A);
-
-        getContentPane().add(mainPanel);
-        setVisible(true);
+        loginDisp.setVisible(true);
+    }
+    class LoginDisp extends JFrame {
+        private static final long serialVersionUID = 1L;
+        LoginDisp(){
+            setContentPane(loginDisp);
+            this.setBounds( 200, 200, 400, 400 );
+            this.setTitle("FakeBook");
+            JLabel text = new JLabel("pass");
+            JButton buttonLogin = new JButton("画面Bへ移動");
+            text.setSize(150, 20);
+            password.setSize(150, 20);
+            buttonLogin.setSize(200, 20);
+            text.setLocation(50, 50);
+            password.setLocation(50, 90);
+            buttonLogin.setLocation(50, 200);
+            ActionListener login = new Login();
+            buttonLogin.addActionListener(login);
+            addComponent(loginDisp, text, buttonLogin, password);
+        }
     }
 
-    private JPanel createPanelA() {
-        JPanel panel = new JPanel(null);
-        JLabel text = new JLabel("pass");
-        JButton goPanelB = new JButton("画面Bへ移動");
-        text.setSize(150, 20);
-        password.setSize(150, 20);
-        goPanelB.setSize(200, 20);
-        text.setLocation(50, 50);
-        password.setLocation(50, 90);
-        goPanelB.setLocation(50, 200);
-        ActionListener login = new Login();
-        goPanelB.addActionListener(login);
-        addComponent(panel, text, goPanelB, password);
-        return panel;
-    }
 
-    private JPanel createPanelB() {
-        JPanel panel=new JPanel();
-        JLabel title = new JLabel("画面B");
-        JButton goPanelA = new JButton("画面Aへ移動");
-        title.setSize(100, 50);
-        goPanelA.setSize(200, 50);
-        title.setLocation(150, 100);
-        goPanelA.setLocation(150, 200);
-        goPanelA.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ((CardLayout)(mainPanel.getLayout())).show(mainPanel, PANEL_A);
+    public class Login extends JButton implements ActionListener {
+        private static final long serialVersionUID = 1L;
+        public Login() {
+            super();
+            this.addActionListener(this);
+        }
+		public void actionPerformed(ActionEvent evt) {
+            String pass = new String(password.getPassword());
+            if(pass.equals("0000")){
+                homeDisp.setVisible(true);
+            } else {
+                JLabel error = new JLabel("パスワードが違います");
+                JOptionPane.showMessageDialog(this, error, "失敗", JOptionPane.ERROR_MESSAGE);     
             }
-        });
-        panel.add(title);
-        panel.add(goPanelA);
-        return panel;
+		}
+    }
+
+    class HomeDisp extends JFrame {
+        private static final long serialVersionUID = 1L;
+        HomeDisp(){
+            this.setVisible(true);
+            this.setBounds( 200, 200, 400, 400 );
+            homeDisp.setLayout( null );
+            this.setTitle("FakeBook");
+            this.setContentPane(homeDisp);
+        }
     }
     void addComponent(JPanel panleName, Component... components){
         for(int i = 0; i < components.length; i++){
