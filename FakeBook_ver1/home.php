dba2073a86241f3093fbba133a1bf6e4a9c3336a
wonder-princess <54895094+wonder-princess@users.noreply.github.com>
2019-09-08 20:27:44 +0900

    Add files via upload

---
+++ FakeBook_ver1/home.php
@@ -0,0 +1,53 @@
+<?php 
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/style.css">
+<title>home</title>
+</head>
+<body>   
+<!--ヘッダー-->
+<header>
+
+    <nav id="always_navi">
+        <ul>
+            <form method="post" action="search.php">
+                <li class="search">検索<input type="text" name="search" id="search"></li>
+            </form>
+            <li class="myPage"><a href="myPage.php">マイページ</a></li>
+        </ul>
+    </nav>
+
+
+<article id="top_cotent">
+        <form method="post" action="post.php">
+            <input type="text" name="search" id="search">
+            <div class="submit_button">
+                <input type="submit" name="submitButton" id="submitButton" value="投稿">
+            </div>
+        </form>
+    <div id="system">
+        <h2>当社のシステム</h2>
+        <ul>
+            <ol><?php echo $val1['']; ?></ol>
+            <ol><?php echo $val2['']; ?></ol>
+            <ol><?php echo $val3['']; ?></ol>
+            <ol><?php echo $val4['']; ?></ol>
+            <ol><?php echo $val5['']; ?></ol>
+            <ol><?php echo $val6['']; ?></ol>
+            <ol><?php echo $val7['']; ?></ol>
+            <ol><?php echo $val8['']; ?></ol>
+            <ol><?php echo $val9['']; ?></ol>
+            <ol><?php echo $val0['']; ?></ol>
+        </ul>
+    </div>
+</article>
+<p id="scrollTop"><a href="#">ページトップ</a></p>
+
+</body>
+</html>
